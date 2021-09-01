effcaa76356af075e1084f4dabb42c27d254b36a
bilal <56475491+Ambush64@users.noreply.github.com>
2021-09-01 12:38:45 +0530

    Add files via upload

---
+++ Website 2/main.js
@@ -0,0 +1,11 @@
+var navlinks = document.getElementById("nav-links")
+
+function showMenu(){
+	navlinks.style.right = "0px";
+	navlinks.style.display = "block"
+}
+
+function hideMenu(){
+	navlinks.style.right = "-200px";
+	navlinks.style.display = "block"
+}
